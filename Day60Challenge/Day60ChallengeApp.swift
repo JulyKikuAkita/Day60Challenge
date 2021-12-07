f6557383622d088adf5debbee7003db08aeb7eb4
JulyKikuAkita <b92701105@gmail.com>
2021-12-06 21:55:03 -0800

    Initial Commit

---
+++ Day60Challenge/Day60ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day60ChallengeApp.swift
+//  Day60Challenge
+//
+//  Created by Ifang Lee on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Day60ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
